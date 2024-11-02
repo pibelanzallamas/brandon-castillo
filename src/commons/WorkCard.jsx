cb5d2b7ae1e0a1e5050810de578480935e3aa7a6
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-11-02 16:54:47 -0300

    works objects almost finished

--- src/commons/WorkCard.jsx
+++ src/commons/WorkCard.jsx
@@ -5,7 +5,6 @@ function WorkCard({ work }) {
     <div className="work-card">
       <p className="work-title">{work.title}</p>
       <p className="work-desc">{work.desc}</p>
-
       {work.photos.length > 0 &&
         work.photos.map(
           (ele, i) =>
@@ -15,28 +14,33 @@ function WorkCard({ work }) {
               </figure>
             )
         )}
-
       <div className="work-links">
         <figure>
-          <img
-            src="https://res.cloudinary.com/daynclfo8/image/upload/v1730563754/Group_3_butzfd.svg"
-            alt={`link for ${work.title}`}
-            srcset=""
-          />
+          <a href={work.url} target="_blank">
+            <img
+              src="https://res.cloudinary.com/daynclfo8/image/upload/v1730563754/Group_3_butzfd.svg"
+              alt={`link for ${work.title}`}
+              srcset=""
+            />
+          </a>
         </figure>
         <figure>
-          <img
-            src="https://res.cloudinary.com/daynclfo8/image/upload/v1730563776/Vector_aejcoc.svg"
-            alt={`github link for ${work.title}`}
-            srcset=""
-          />
+          <a href={work.gh} target="_blank">
+            <img
+              src="https://res.cloudinary.com/daynclfo8/image/upload/v1730563776/Vector_aejcoc.svg"
+              alt={`github link for ${work.title}`}
+              srcset=""
+            />
+          </a>
         </figure>
         <figure>
-          <img
-            src="https://res.cloudinary.com/daynclfo8/image/upload/v1730563767/Group_1_kqupq1.svg"
-            alt={`youtube link for ${work.title}`}
-            srcset=""
-          />
+          <a href={work.video} target="_blank">
+            <img
+              src="https://res.cloudinary.com/daynclfo8/image/upload/v1730563767/Group_1_kqupq1.svg"
+              alt={`youtube link for ${work.title}`}
+              srcset=""
+            />
+          </a>
         </figure>
       </div>
     </div>
