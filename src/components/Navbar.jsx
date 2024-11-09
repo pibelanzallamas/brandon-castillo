f43703b6d52a99d138777d9a91f931636a4e10c6
Brandon Castillo <brandoncastillo.09@gmail.com>
2024-11-09 11:51:34 -0300

    hambu ready

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,22 +9,60 @@ function Navbar() {
     { to: "/projects", title: "Projects" },
     { to: "/aboutme", title: "About Me" },
     { to: "/services", title: "Services" },
-    // {to: "/",}
   ];
+
+  const [hambu, setHambu] = useState(false);
+
+  const handleHambu = () => {
+    setHambu(!hambu);
+  };
+
   return (
-    <nav>
-      <h1>
-        <Link to={"/"}>Brandon Castillo</Link>
-      </h1>
-      <ul>
-        {links.length > 0 &&
-          links.map((ele, i) => (
-            <Link to={ele.to} onClick={() => setLink(i)} key={i}>
-              {link == i ? <u>{ele.title}</u> : ele.title}
-            </Link>
-          ))}
-      </ul>
-    </nav>
+    //compo mobile y otro para desktop
+    //
+
+    <>
+      <nav className="nav nav-desktop">
+        <h1>
+          <Link to={"/"}>Brandon Castillo</Link>
+        </h1>
+        <ul>
+          {links.length > 0 &&
+            links.map((ele, i) => (
+              <Link to={ele.to} onClick={() => setLink(i)} key={i}>
+                {link == i ? <u>{ele.title}</u> : ele.title}
+              </Link>
+            ))}
+        </ul>
+      </nav>
+      <nav className="nav nav-mobile">
+        <h1>
+          <Link to={"/"}>Brandon Castillo</Link>
+        </h1>
+        <a onClick={handleHambu}>
+          <figure>
+            <img
+              src={
+                !hambu
+                  ? "https://res.cloudinary.com/daynclfo8/image/upload/v1731162233/menu_h06lbd.svg"
+                  : "https://res.cloudinary.com/daynclfo8/image/upload/v1731163324/x_lqjkvf.svg"
+              }
+              alt="hamburger-icon"
+            ></img>
+          </figure>
+        </a>
+      </nav>
+      <div className={hambu ? "hambu" : "notHambu"}>
+        <ul>
+          {links.length > 0 &&
+            links.map((ele, i) => (
+              <Link to={ele.to} onClick={() => setLink(i)} key={i}>
+                {link == i ? <u>{ele.title}</u> : ele.title}
+              </Link>
+            ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
