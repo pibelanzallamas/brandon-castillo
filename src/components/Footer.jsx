fbd494f4131b389aaa14474113fc201d16111725
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-15 16:03:06 -0300

    underline for footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function Footer() {
+  const links = [
+    { to: "/education", title: "Education" },
+    { to: "/experience", title: "Experience" },
+    { to: "/works", title: "Works" },
+    { to: "/projects", title: "Projects" },
+    { to: "/services", title: "Services" },
+    { to: "/contact", title: "Contact" },
+  ];
+  const [link, setLink] = useState("");
   return (
     <footer>
       <h3>Menu</h3>
       <div className="footer-links">
-        <Link to="/education">Education</Link>
-        <Link to="/experience">Experience</Link>
-        <Link to="/works">Works</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
+        {links.length > 0 &&
+          links.map((ele, i) => (
+            <Link to={ele.to} onClick={() => setLink(ele.title)} key={i}>
+              {link == ele.title ? <u>{ele.title}</u> : ele.title}
+            </Link>
+          ))}
       </div>
     </footer>
   );
