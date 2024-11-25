e4f9b2a0e5b1ba4e5d986338233402a44c4bb484
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-25 12:05:43 -0300

    works page update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,43 +1,40 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
 import works from "../utils/works";
-import WorkCard from "../commons/WorkCard";
+import ExpCard from "../commons/ExpCard";
 
 function Works() {
-  // const dispatch = useDispatch();
-  // const esp = useSelector((state) => state.lang.esp);
-  const [selWork, setSelWork] = useState(works[0]);
-  const [link, setLink] = useState(() => {
-    const localStg = localStorage.getItem("link");
-    return localStg ? JSON.parse(localStg) : -1;
-  });
-  const [isChanged, setIsChanged] = useState(false);
+  const [selExp, setSelExp] = useState(works[0]);
+  const [changeItem, setChangeItem] = useState(false);
 
-  useEffect(() => {
-    localStorage.setItem("link", JSON.stringify(link));
-    setIsChanged(!isChanged);
-  }, [link]);
+  // ?
+  // useEffect(() => {
+  //   localStorage.setItem("link", JSON.stringify(link));
+  //   setIsChanged(!isChanged);
+  // }, [link]);
 
-  useEffect(() => {
-    setLink(0);
-  }, []);
+  // useEffect(() => {
+  //   setLink(0);
+  // }, []);
 
   return (
-    <main className="work-content">
-      <ul className="side-list">
-        {works.map((work, i) => (
+    <main className="edu">
+      <h3>Works</h3>
+      {/* titulos */}
+      <div className="front-page-links footer-links">
+        {works.map((ex, i) => (
           <a
             key={i}
             onClick={() => {
-              setSelWork(work);
-              setLink(i);
+              setSelExp(ex);
+              setChangeItem(!changeItem);
             }}
+            href="#"
           >
-            {link === i ? <u>{work.title}</u> : work.title}
+            {selExp.title == ex.title ? <u>{ex.title}</u> : ex.title}
           </a>
         ))}
-      </ul>
-      <WorkCard work={selWork} changed={isChanged} />
+      </div>
+      <ExpCard card={selExp} disparador={changeItem} />
     </main>
   );
 }
