8788bccdec924813cd8a2da51c7141e65a66f4c9
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-27 12:33:32 -0300

    service update

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,33 +5,34 @@ function Services() {
     <main className="edu">
       <h3>Services</h3>
 
-      <div className="tech-lists">
+      <div className="services-lists">
         <section className="service">
+          <h4>Back End Developer</h4>
           <figure className="service-image">
             <img src="https://ddi-dev.com/uploads/backend-is.png"></img>
           </figure>
 
-          <h4>Back End Developer</h4>
+          <h4>Main Functions:</h4>
           <ul>
             <li>Designing Databases</li>
-            <li>Creating Rest API's</li>
+            <li>Creating REST APIs</li>
             <li>Using External APIs</li>
             <li>Designing Business Logic</li>
-            <li>Comunication with DBs</li>
+            <li>Communication with DBs</li>
             <li>Authentication</li>
           </ul>
         </section>
         <section className="service">
+          <h4>Front End Developer</h4>
           <figure className="service-image">
             <img src="https://res.cloudinary.com/proxify-io/image/upload/c_fit,f_auto,q_auto,dpr_auto,w_900/v1/cms/images/gallery/xizwtpiwqewW9qZVGgeXy5Ee0kOc8MAHrkFwEXtm.png"></img>
           </figure>
-
-          <h4>Front End Developer</h4>
+          <h4>Main Functions:</h4>
           <ul>
-            <li>Designing Attractive Websites</li>
+            <li>Designing Attractive User Interfaces</li>
             <li>Connecting to APIs</li>
             <li>Responsive Design</li>
-            <li>Design to Code in simple steps</li>
+            <li>Turning Designs into Code</li>
             <li>Multiple frameworks, libraries and packages</li>
           </ul>
         </section>
