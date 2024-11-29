dc1e427bc3869ca3f3f05eccbca39d9359092028
pibelanzallamas <brandoncastillo.09@gmail.com>
2024-11-28 21:10:19 -0300

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./styles/app.css";
 import AppRoutes from "./components/AppRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
+      <Analytics />
       <Navbar />
       <div className="content">
         <AppRoutes />
