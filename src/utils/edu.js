a1ea5f477b47a6f8e5f688b003de7f45ce0d7816
Brandon Castillo <108550263+pibelanzallamas@users.noreply.github.com>
2025-06-03 23:24:16 -0300

    Update edu.js

--- src/utils/edu.js
+++ src/utils/edu.js
@@ -1,4 +1,17 @@
 const edu = [
+   {
+    title: "National Technological University (UTN)",
+    carrera: "Technical University Degree in Programming",
+    inicio: "Mar 2025",
+    final: "Dec 2027",
+    res: "1600 /1067",
+    encurso: false,
+    desc: "I completed the first semester of the Technical University Degree in Programming at UTN, where I gained foundational knowledge in programming, data structures, and computational logic. During this period, I worked with languages like C and SQL, developing skills in problem-solving and creating efficient solutions in computing environments. This learning experience provided me with a solid foundation in software development and motivated me to continue specializing in modern technologies like Full Stack development.",
+    photos: [
+      "https://extensionfra.com.ar/wp-content/uploads/2018/06/DSC_0102-scaled.jpg",
+      "https://utn.edu.ar/images/Secretarias/ACYPI/2021/utn-mdp.png",
+    ],
+  },
   {
     title: "Plataforma 5",
     carrera: "Full Stack Developer Bootcamp",
@@ -12,30 +25,15 @@ const edu = [
     ],
   },
   {
-    title: "National Technological University (UTN)",
-    carrera: "Technical University Degree in Programming",
-    inicio: "Jul 2022",
-    final: "Dec 2022",
-    res: "1600 /1067",
-    encurso: false,
-    desc: "I completed the first semester of the Technical University Degree in Programming at UTN, where I gained foundational knowledge in programming, data structures, and computational logic. During this period, I worked with languages like C and SQL, developing skills in problem-solving and creating efficient solutions in computing environments. This learning experience provided me with a solid foundation in software development and motivated me to continue specializing in modern technologies like Full Stack development.",
-    photos: [
-      "https://extensionfra.com.ar/wp-content/uploads/2018/06/DSC_0102-scaled.jpg",
-      "https://utn.edu.ar/images/Secretarias/ACYPI/2021/utn-mdp.png",
-    ],
-  },
-  {
     title: "National University of Lanús (UNLa)",
     carrera: "Bachelor's Degree in Audiovisual Arts",
     inicio: "Feb 2016",
     final: "Dec 2020",
-    res: "1200 / 675",
+    res: "1599 / 1066",
     encurso: false,
     desc: "I completed four semesters of the Bachelor's in Audiovisual Arts at the National University of Lanús (UNLa) in Buenos Aires, where I gained key knowledge in audiovisual production, video and sound editing, and visual storytelling. During this time, I worked on practical projects that integrated creativity and technical skills, exploring areas such as screenwriting, photography, and sound design. This experience allowed me to develop a comprehensive understanding of audiovisual language and the technological tools necessary for multimedia content creation, skills that complement my professional background as a developer.",
     photos: [
-      "https://media.infocielo.com/p/fea022a84a4903bf4a9f889b08789446/adjuntos/299/imagenes/001/432/0001432171/1200x675/smart/unla-3jpg.jpg",
-      "https://www.lanus.gob.ar/storage/fichas/imagenes/1-unla-kB8iT.jpg",
-      "https://filosofia.net/cdf/uhm/unl.png",
+      "https://res.cloudinary.com/daynclfo8/image/upload/v1749002722/unla-pic-building_a3lakr.jpg"
     ],
   },
   {
@@ -44,11 +42,11 @@ const edu = [
     carrera: "Cinema, Subject, and Society ",
     inicio: "Mar 2014",
     final: "Ago 2014",
-    res: "1000 / 566",
+    res: "768 / 366",
     encurso: false,
     desc: "I participated in the Cine, Subject, and Society course at the National School of Cinematographic Experimentation and Realization (ENERC) in Buenos Aires. This course allowed me to explore how cinema is closely linked to the construction of the subject and social dynamics, addressing issues of identity, power, and representation on screen. Through a theoretical and practical approach, I learned to analyze films from a critical perspective, identifying the social processes that influence the creation and reception of cinema. The course also delved into the evolution of cinematic discourses and their impact on contemporary society.",
     photos: [
-      "https://www.diario22.ar/notix2/multimedia/imagenes/fotos/2017-12-20/422466.jpg_gde.jpg",
+      "https://res.cloudinary.com/daynclfo8/image/upload/v1749002708/enerc-building_uww6op.jpg",
     ],
   },
   {
