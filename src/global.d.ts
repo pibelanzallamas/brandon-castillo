ee1dbf1e98c102320d0cb1426668620e39abfa2d
brandoncastillodev <brandoncastillo.09@gmail.com>
2026-04-07 14:43:48 -0300

    fixing ts formats imports

--- src/global.d.ts
+++ src/global.d.ts
@@ -4,3 +4,33 @@ declare module "*.pdf" {
   const file: string
   export default file
 }
+
+declare module '*.jpg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.jpeg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.png' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.webp' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.svg' {
+  const value: string;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: string;
+  export default value;
+}
