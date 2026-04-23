5a1ac9cac028e1354da97576955945726f2dd464
brandoncastillodev <brandoncastillo.09@gmail.com>
2026-04-23 11:02:50 -0300

    new breakpo main width

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import Main from "./components/Main";
 function App() {
   return (
     <div className="layout">
-      <Analytics />
-      <Header />
-      <div className="content" style={{ flex: "1" }}>
-        <Main/>
+      <div className="tye">
+        <Analytics />
+        <Header />
+        <div className="content" style={{ flex: "1" }}>
+          <Main/>
+        </div>
       </div>
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,45 +1,9 @@
-import { useState, useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
 
 function Footer() {
-  const location = useLocation();
-  const [link, setLink] = useState("");
-
-  const links = [
-    { to: "/education", title: "Education" },
-    // { to: "/experience", title: "Experience" },
-    { to: "/work", title: "Work" },
-    // { to: "/projects", title: "Projects" },
-    // { to: "/services", title: "Services" },
-    { to: "/contact", title: "Contact" },
-  ];
-
-  useEffect(() => {
-    if (location.pathname === "/") {
-      setLink("");
-    }
-
-    const timer = setTimeout(() => {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }, 100);
-
-    return () => clearTimeout(timer);
-  }, [location]);
-
   return (
     <footer>
-      {/* <h3>More About</h3> */}
       <div className="footer-links">
-        {/* {links.length > 0 &&
-          links.map((ele, i) => (
-          <Link to={ele.to} onClick={() => setLink(ele.title)} key={i}>
-            {link == ele.title ? <u>{ele.title}</u> : ele.title}
-          </Link>
-        ))} */}
-          <p class="firma">Made with ❤️ by Brandon 🏰</p>
+          <p className="firma">Made with ❤️ by Brandon 🏰</p>
       </div>
     </footer>
   );
